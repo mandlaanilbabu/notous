f2e1e5cfdf2d8feb71923fe230557f081b1344ed
anilbabu <mandla.anilbabu@gmail.com>
2013-02-21 17:25:02 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notous::Application.config.secret_token = '76a9bcf6b052d061c9c3c35f95838ec1fa2159afb816531e649ad0711b2b663f765c795e2d4bb4b131396130851d2197a2acfac904549e7db0ea210a6a3ae0ab'
